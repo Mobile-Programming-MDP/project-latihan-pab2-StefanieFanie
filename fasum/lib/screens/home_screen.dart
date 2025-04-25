8ecc8c96460847ce993f5ae708fb58be4a891473
StefanieFanie <stefanie_2226240009@mhs.mdp.ac.id>
2025-04-25 20:32:44 +0700

    ADD : add post screen

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fasum/screens/add_post_screen.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -47,48 +49,85 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("posts").snapshots(),
-        // .orderBy('createdAt', descending: true).snapshots(),
+        stream: FirebaseFirestore.instance.collection("posts").orderBy('createdAt', descending: true).snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
             return Center(child: CircularProgressIndicator());
 
             final posts = snapshot.data!.docs;
             return ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                final data = posts[index].data();
-                final imageBase64 = data['image'];
-                final description = data['description'];
-                // final createdAtStr = data['createdAt'];
-                final fullName = data['fullName'] ?? 'Anonim';
-
-                // Parse ke DateTime
-                // final createdAt = DateTime.parse(createdAtStr);
-                return Card(
-                  margin: const EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        fullName,
-                        style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final data = posts[index].data();
+              final imageBase64 = data['image'];
+              final description = data['description'];
+              final createdAtStr = data['createdAt'];
+              final fullName = data['fullName'] ?? 'Anonim';
+ 
+              //parse ke DateTime
+              final createdAt = DateTime.parse(createdAtStr);
+              return Card(
+                margin: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (imageBase64 != null)
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(10)),
+                        child: Image.memory(base64Decode(imageBase64),
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200),
                       ),
-                      const SizedBox(height: 6),
-                      Text(
-                        description ?? '',
-                        style: const TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                );
-              }
-            );
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                formatTime(createdAt),
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey),
+                              ),
+                              Text(
+                                fullName,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(height: 6),
+                            ],
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            description ?? '',
+                            style: const TextStyle(fontSize: 16),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
         }
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => AddPostScreen()),
+          );
+        },
         child: const Icon(Icons.add),
       ),
     );
